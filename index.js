c137ddbf03f5b0c8d8f580f457be75498c3273f4
serophiom <serophiom@gmail.com>
2021-05-22 09:50:27 +0300

    добавляет переключение цветов

--- index.js
+++ index.js
@@ -7,3 +7,42 @@ const colors = [
     '#795548',
   ];
   
+const bodyRef = document.querySelector('body');
+const startBtnRef = document.querySelector('button[data-action="start"]');
+const stoptBtnRef = document.querySelector('button[data-action="stop"]');
+console.log(startBtnRef);
+console.log(bodyRef);
+let colorSwithInterval;
+const randomIntegerFromInterval = (min, max) => {
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
+
+// setInterval(() => {
+//   console.log('Смена цвета');
+// }, 1000);
+
+
+
+startBtnRef.addEventListener('click', ()  => {
+  colorSwithInterval = setInterval(() => {
+
+    console.log('Смена цвета');
+    bodyRef.style.backgroundColor = colors[randomIntegerFromInterval(0, colors.length-1)];
+  }, 1000);
+});
+
+stoptBtnRef.addEventListener('click', ()  => {
+  clearInterval(colorSwithInterval);
+});
+
+
+// 'setInterval(colorSwith, 1000)'
+// )
+ 
+
+// function colorSwith (colors) {
+//   bodyRef.className = randomIntegerFromInterval;
+// }
+
+
+
